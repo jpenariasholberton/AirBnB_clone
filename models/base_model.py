4ad42b2ba19e8aa43a3e945017c601d4407649c4
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-11-02 14:41:54 +0000

    update

--- models/base_model.py
+++ models/base_model.py
@@ -4,35 +4,53 @@ Class Base
 """
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel():
     """
-    main class BaseModel to take cara of initialization, serialization,
+    Main class BaseModel to take cara of initialization, serialization,
     and deserializatoin of your future instances
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
+        Initialization
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+        else:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strftime(
+                                                    "%Y-%m-%dT%H:%M:%S.%f"))
+                elif not key == "__class__":
+               ls     setattr(self, key, value)
 
     def __str__(self):
         """
+        String representation of an object
         """
-        return "[{}] ({}) {}".format(self.__name__.__class__, self.id, self.__dict__)
+        return "[{}] ({}) {}".\
+            format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """
+        Updates the public instance attribute updated_at with
+        the current datetime
         """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """
+        Returns a dictionary containing all keys/values
+        of __dict__ of an instance
         """
-        dic = dict(self.__dict__)
+        dic = self.__dict__.copy()
         dic["__class__"] = self.__class__.__name__
-        dic["created_at"] = self.created_at.isoformat()
-        dic["updated_at"] = self.updated_at.isoformat()
+        dic["created_at"] = dic["created_at"].isoformat()
+        dic["updated_at"] = dic["updated_at"].isoformat()
         return dic
